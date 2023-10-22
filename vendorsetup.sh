006be6a8ab30821c2c7433f6df3595c68edf7118
Sakura桜 <63789876+SakuraKyuo@users.noreply.github.com>
2023-10-22 23:29:09 +0800

    grus: switch hardware_xiaomi from LineageOS to EvolutionX

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,5 +8,5 @@ rm -rf device/xiaomi/grus/prebuilt/GoogleCameraENG
 git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sdm710-common device/xiaomi/sdm710-common -b UDC
 git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi -b UDC
 git clone --depth=1 https://github.com/SakuraNotStupid/android_kernel_xiaomi_sdm710 kernel/xiaomi/sdm710
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/Evolution-X/hardware_xiaomi -b udc hardware/xiaomi
 git clone https://gitlab.com/SakuraNotStupid/GoogleCameraENG.git device/xiaomi/grus/prebuilt/GoogleCameraENG
