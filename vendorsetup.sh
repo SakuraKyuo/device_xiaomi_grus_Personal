d05b1c714864ea2a59d7ff94140f53a8044b4ea1
Sakura桜 <63789876+SakuraKyuo@users.noreply.github.com>
2023-10-24 00:00:17 +0800

    grus: drop clone hardware_xiaomi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,5 +8,4 @@ rm -rf device/xiaomi/grus/prebuilt/GoogleCameraENG
 git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sdm710-common device/xiaomi/sdm710-common -b UDC
 git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi -b UDC
 git clone --depth=1 https://github.com/SakuraNotStupid/android_kernel_xiaomi_sdm710 kernel/xiaomi/sdm710
-git clone https://github.com/Evolution-X/hardware_xiaomi -b udc hardware/xiaomi
 git clone https://gitlab.com/SakuraNotStupid/GoogleCameraENG.git device/xiaomi/grus/prebuilt/GoogleCameraENG
