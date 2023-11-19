56866d4a52870bff92f495b2c2324b7f4f32fe51
Sakura桜 <63789876+SakuraKyuo@users.noreply.github.com>
2023-11-19 17:06:58 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,10 @@
 # nuke
 rm -rf device/xiaomi/sdm710-common
-rm -rf vendor/xiaomi/grus
-rm -rf vendor/xiaomi/sdm710-common
-rm -rf vendor/xiaomi-backup
+rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 rm -rf hardware/xiaomi
 # clone
 git clone https://github.com/SakuraKyuo/device_xiaomi_sdm710-common_Personalon device/xiaomi/sdm710-common -b UDC
-mv vendor/xiaomi/ vendor/xiaomi-backup/
 git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_grus vendor/xiaomi -b UDC
-mv vendor/xiaomi-backup/* vendor/xiaomi/
 git clone --depth=1 https://github.com/SakuraNotStupid/android_kernel_xiaomi_sdm710 kernel/xiaomi/sdm710 -b UDC
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
